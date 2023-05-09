67529499ba394104ce1056d4c80d0130fd72649c
Dmitry D <dvdrelin@gmail.com>
2023-05-10 01:54:14 +0300

    string builder .Append vs new string() .AsSpan .Slice

---
+++ StringBenchmarks/AnotherOneStringBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System.Buffers;
+
+public class AnotherOneStringBuilder : IDisposable
+{
+    private int _alreadyIn;
+    private char[]? _chars;
+    private string? _alreadyBuilt;
+
+    public AnotherOneStringBuilder()
+    {
+        _chars = ArrayPool<char>.Shared.Rent(ushort.MaxValue*byte.MaxValue);
+    }
+
+    public AnotherOneStringBuilder Append(string value)
+    {
+        _alreadyBuilt = null;
+        value.AsSpan().CopyTo(_chars!.AsSpan()[_alreadyIn..]);
+        _alreadyIn += value.Length;
+        return this;
+    }
+
+    public override string ToString() => _alreadyBuilt ??= new(_chars!, 0, _alreadyIn);
+
+    public void Dispose()
+    {
+        if (_chars == null) return;
+        ArrayPool<char>.Shared.Return(_chars!);
+        _chars = null;
+        _alreadyBuilt = null;
+        _alreadyIn = 0;
+    }
+}
---
+++ StringBenchmarks/Benchmark.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+
+[MemoryDiagnoser]
+public class Benchmark
+{
+    
+    public IEnumerable<string[]> Values()
+    {
+        yield return new[] { "Hello", ", World!", "\r\n", "My ", "Name ", "is", " Dima", "!" };
+        yield return Enumerable.Range(0, 33).Select(_ => "abcdefghijklmnopqrstuvwxyz").ToArray();
+        yield return Enumerable.Range(0, 100).Select(_ => "abcdefghijklmnopqrstuvwxyz").ToArray();
+        //yield return Enumerable.Range(0, 300).Select(_ => "abcdefghijklmnopqrstuvwxyz").ToArray();
+        //yield return Enumerable.Range(0, 700).Select(_ => "abcdefghijklmnopqrstuvwxyz").ToArray();
+        //yield return Enumerable.Range(0, 1000).Select(x => new string('a',x)).ToArray();
+    }
+
+    [Benchmark] [ArgumentsSource(nameof(Values))]
+    public string Test_StringBuilder(string[] values)
+    {
+        var sb = new StringBuilder();
+         foreach (var value in values) sb.Append(value);
+        return sb.ToString();
+    }
+
+    [Benchmark] [ArgumentsSource(nameof(Values))]
+    public string Test_AnotherOneStringBuilder(string[] values)
+    {
+        using var sb = new AnotherOneStringBuilder();
+        foreach (var value in values) sb.Append(value);
+        return sb.ToString();
+    }
+}
