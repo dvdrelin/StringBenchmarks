6158b4b8cc94eec8dd0fc18d861b88339e107df0
dvdrelin <dvdrelin@gmail.com>
2023-05-10 11:10:10 +0300

    benchmark arguments gitignore

--- StringBenchmarks/Benchmark.cs
+++ StringBenchmarks/Benchmark.cs
@@ -10,16 +10,14 @@ public class Benchmark
         yield return new[] { "Hello", ", World!", "\r\n", "My ", "Name ", "is", " Dima", "!" };
         yield return Enumerable.Range(0, 33).Select(_ => "abcdefghijklmnopqrstuvwxyz").ToArray();
         yield return Enumerable.Range(0, 100).Select(_ => "abcdefghijklmnopqrstuvwxyz").ToArray();
-        //yield return Enumerable.Range(0, 300).Select(_ => "abcdefghijklmnopqrstuvwxyz").ToArray();
-        //yield return Enumerable.Range(0, 700).Select(_ => "abcdefghijklmnopqrstuvwxyz").ToArray();
-        //yield return Enumerable.Range(0, 1000).Select(x => new string('a',x)).ToArray();
+        yield return Enumerable.Range(0, 1000).Select(x => new string('a',x)).ToArray();
     }
 
     [Benchmark] [ArgumentsSource(nameof(Values))]
     public string Test_StringBuilder(string[] values)
     {
         var sb = new StringBuilder();
-         foreach (var value in values) sb.Append(value);
+        foreach (var value in values) sb.Append(value);
         return sb.ToString();
     }
 
